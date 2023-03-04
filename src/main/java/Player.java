6a742bf83930087f1dfdea9667033fe6b8cb79af
Abraham Alex Otoo <alexotoous@outlook.com>
2023-03-04 07:07:02 +0000

    constructors done

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -15,7 +15,7 @@
  **/
 public class Player {
     public String playerName;
-    public int level;
+    public int level =1;
 
     /**
      * this.playerName should be set to playerName.
@@ -31,6 +31,7 @@ public class Player {
      * @param playerName the name of the player to be created.
      */
     public Player(String playerName){
+        this.playerName = playerName;
 
     }
 
@@ -48,6 +49,8 @@ public class Player {
      * @param level the starting level of the player to be created.
      */
     public Player(String playerName, int level){
+        this.playerName = playerName;
+        this.level = level;
 
     }
 
